701b91648a3b5dca70e1ab7280fb614097a121e3
Jan Pokorný <pokorj54@fit.cvut.cz>
2020-03-31 01:06:47 +0200

    whitespace fix

--- src/main/kotlin/dev/vrba/BoomerImage.kt
+++ src/main/kotlin/dev/vrba/BoomerImage.kt
@@ -6,7 +6,7 @@ object BoomerImage
         "https://i.imgur.com/mllwVTT.gif",
         "https://i.imgur.com/vRRzfwB.png",
         "https://i.imgur.com/dtjDrD8.png",
-	"https://i.imgur.com/mYQBAro.png"
+        "https://i.imgur.com/mYQBAro.png"
     )
     
     private val repeatedBoomerPostingImages = listOf(
@@ -17,7 +17,7 @@ object BoomerImage
         "https://i.imgur.com/MPw3EYt.png",
         "https://i.imgur.com/9Y9oW3G.png",
         "https://i.imgur.com/kt9H1mE.png",
-	"https://i.imgur.com/kFp4zTY.png"
+        "https://i.imgur.com/kFp4zTY.png"
     )
     
     val forIntroduction: String
