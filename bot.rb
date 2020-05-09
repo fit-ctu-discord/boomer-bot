3b6e80d211cade6ffd01184f6d508a7462ee2486
Jiří Vrba <jiri@vrba.dev>
2020-05-09 20:29:50 +0200

    Config: use symbols instead of strings as the hash keys

--- bot.rb
+++ bot.rb
@@ -1,1 +1,14 @@
-# TODO
+require_relative "config.rb"
+
+unless defined? BoomerBot::CONFIG
+  puts "Did you copied the config.example.rb file to config.rb?"
+  puts "Cannot find BoomerBot::CONFIG, exiting."
+  exit 1
+end
+
+unless defined? BoomerBot::DISCORD_TOKEN
+  puts "Cannot find BoomerBot::DISCORD_TOKEN, exiting."
+  exit 1
+end
+
+require_relative "src/client"
--- config.example.rb
+++ config.example.rb
@@ -1,20 +1,23 @@
 module BoomerBot
 
+  # The bot token, that is used to identify the client
+  DISCORD_TOKEN = ""
+
   CONFIG = {
       # From where to yeet
-      meme_channel_id => 0,
+      :meme_channel_id => 0,
       # Where to yeet
-      boomers_channel_id => 0,
+      :boomers_channel_id => 0,
 
       # Id of the meme master role, that is not affected by boomer emotes
-      meme_master_role_id => 0,
+      :meme_master_role_id => 0,
       # Id of the boomer role (formerly the Clown role)
-      boomer_role_id => 0,
+      :boomer_role_id => 0,
 
       # Id of the boomer emote, that the bot should react to
-      boomer_emote_id => 0,
+      :boomer_emote_id => 0,
       # The number of boomer emotes required to trigger the bot
-      boomer_emote_threshold => 0,
+      :boomer_emote_threshold => 0,
   }
 
 end
