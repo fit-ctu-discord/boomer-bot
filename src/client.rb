f3590a7db2689171460f450655ad9fe79f1a7dba
Jiří Vrba <jiri@vrba.dev>
2020-05-10 09:04:30 +0200

    Show some love to the bot!

--- src/client.rb
+++ src/client.rb
@@ -1,5 +1,6 @@
 require 'discordrb'
 require_relative 'boomer_emote_listener'
+require_relative 'good_bot_listener'
 
 bot = Discordrb::Bot.new token: BoomerBot::DISCORD_TOKEN
 
@@ -7,6 +8,7 @@ puts 'Created the bot instance'
 puts "Invite url: #{bot.invite_url permission_bits: 268822592}"
 
 BoomerBot::Listeners::BoomerEmoteAddedListener.new(bot).register
+BoomerBot::Listeners::GoodBotListener.new(bot).register
 
 bot.run
 
---
+++ src/good_bot_listener.rb
@@ -0,0 +1,25 @@
+module BoomerBot
+  module Listeners
+
+    class GoodBotListener
+
+      def initialize(bot)
+        @bot = bot
+        @config = BoomerBot::CONFIG
+        @boomer_channel = @bot.channel @config[:boomers_channel_id]
+      end
+
+      def register
+        @bot.message(in: @boomer_channel) do |event|
+          message = event.message
+          # The bot really likes to hear that he is good, best etc.
+          if message.content.match(/^(?:[Gg]ood|[Bb]est|[Tt]he best) bot(?: ever)?/)
+            # So he will say his thank you through the heart emote
+            message.create_reaction '🤖'
+            message.create_reaction %w[❤️ 💕 💖].sample
+          end
+        end
+      end
+    end
+  end
+end
