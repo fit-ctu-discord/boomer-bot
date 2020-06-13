d1bfcbbe4733ecb33117e39b675c713a5a7690a7
Jiří Vrba <jiri@vrba.dev>
2020-06-13 23:45:33 +0200

    Oh, those russians

--- src/xd_messages_listener.rb
+++ src/xd_messages_listener.rb
@@ -19,7 +19,7 @@ module BoomerBot
 
                     message = event.message
                     
-                    pattern = /(?:(?:x|🇽)+\s*(?:d|🇩)+(?:[\s\W]|$)|[ie]ks\s*d[eéý]?)/i
+                    pattern = /(?:(?:x|🇽|х)+\s*(?:d|🇩)+(?:[\s\W]|$)|[ie]ks\s*d[eéý]?)/i
 
                     if message.content.match pattern
                         react_with_boomer event
